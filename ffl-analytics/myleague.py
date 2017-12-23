9555cd1cca27a89ecfd20be215b16c5562c76239
Ryan Hallisey <rthallisey@gmail.com>
2017-12-23 13:20:13 -0500

    Remove extra dir

---
+++ ffl-analytics/myleague.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+PROJECT_ROOT = os.path.abspath(os.path.join(
+        os.path.dirname(os.path.realpath(__file__))))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+import league
+
+league_id = 361793
+season_id = 2017
+L = league.League(league_id, season_id)
+print L
+
+teams = L.get_teams()
+
+fitz = teams[8]
+print fitz.get_roster(1)
+print fitz.get_roster()
+
+for team in teams:
+    print team.name
+    print team.player_changes(1)
